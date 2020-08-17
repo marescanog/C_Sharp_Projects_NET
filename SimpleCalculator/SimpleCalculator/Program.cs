592a7f9be76689619247130a76a297783219a0cb
Marvie Gasataya <marescanog@gmail.com>
2020-08-17 20:23:01 +0800

    added CalculatorEngine class

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -7,15 +7,15 @@ namespace SimpleCalculator
         static void Main(string[] args)
         {
             InputConverter inputConverter = new InputConverter();
-            //CalculatorEngine calculatorEngine = new CalculatorEngine();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
 
             double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
             double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
             string operation = Console.ReadLine();
 
-            //double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-            //Console.WriteLine(result);
+            Console.WriteLine(result);
         }
     }
 }
