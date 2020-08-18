8ba1ac78b502aea9e6c672f5dbaec726cb2f5282
Marvie Gasataya <marescanog@gmail.com>
2020-08-18 19:34:15 +0800

    Changed framework unit testing to core

--- SimpleCalculator/SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator/SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
 
 namespace SimpleCalculator.Test.Unit
 {
-    [TestClass]
     public class CalculatorEngineTest
     {
-        [TestMethod]
-        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+        [Fact]
+        public void AddTwoNumbersAndReturnsValidResultForNonSymbolOperations()
         {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+            Assert.Equal(3, result);
         }
     }
 }
