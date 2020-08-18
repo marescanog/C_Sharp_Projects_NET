87a17942235b2885759777b9dfd6d77b30fd1588
Marvie Gasataya <marescanog@gmail.com>
2020-08-18 18:38:31 +0800

    Added exception handling

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -6,16 +6,25 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            InputConverter inputConverter = new InputConverter();
-            CalculatorEngine calculatorEngine = new CalculatorEngine();
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
 
-            double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-            double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-            string operation = Console.ReadLine();
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
 
-            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-            Console.WriteLine(result);
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                //In real world we would want to log this message
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
