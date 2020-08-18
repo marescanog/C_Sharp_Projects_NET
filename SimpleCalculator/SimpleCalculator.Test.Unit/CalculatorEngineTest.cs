d6906cb33cb8e5ebb4ffc05457e35e0d53096a47
Marvie Gasataya <marescanog@gmail.com>
2020-08-18 18:57:09 +0800

    Added Unit testing

---
+++ SimpleCalculator/SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+        }
+    }
+}
