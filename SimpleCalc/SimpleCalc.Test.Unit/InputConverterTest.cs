3320de47518a7c414bda625cdef37664f8d2ed41
Marvie Gasataya <marescanog@gmail.com>
2020-08-21 23:15:13 +0800

    Added Test for ExpectedException

---
+++ SimpleCalc/SimpleCalc.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalc.Test.Unit
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        private readonly InputConverter _iInputConverter = new InputConverter();
+
+        [TestMethod]
+        public void ConvertsValidStringInputIntoDouble()
+        {
+            string inputNumber = "5";
+            double convertedNumber = _iInputConverter.ConvertInputToNumeric(inputNumber);
+            Assert.AreEqual(5, convertedNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailsToConvertInvalidStringInputIntoDouble()
+        {
+            string inputNumber = "*";
+            double convertedNumber = _iInputConverter.ConvertInputToNumeric(inputNumber);
+        }
+    }
+}
