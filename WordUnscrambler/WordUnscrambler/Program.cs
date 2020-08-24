6a70ad63ae09cddd292d2c81da2e65a0aba681a5
Marvie Gasataya <marescanog@gmail.com>
2020-08-24 11:08:22 +0800

    Created Menu Driver for Program

--- WordUnscrambler/WordUnscrambler/Program.cs
+++ WordUnscrambler/WordUnscrambler/Program.cs
@@ -10,6 +10,49 @@ namespace WordUnscrambler
     {
         static void Main(string[] args)
         {
+            var continueWordUnscramble = "Y";
+
+            do
+            {
+                Console.WriteLine("Please enter the option F for File and M for Manual");
+
+                string option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter Scrambled Words File Name: ");
+                        ExecuteScrambledWordsInFileScenario();
+                        break;
+                    case "M":
+                        ExecuteScrambledWordsManualEntryScenario();
+                        Console.Write("Enter Scrambled Words Manually: ");
+
+                        break;
+                    default:
+                        Console.WriteLine("Option was not recognized.");
+                        break;
+                }
+
+                do
+                {
+                    Console.Write("Would you like to continue? Y/N ");
+                    continueWordUnscramble = (Console.ReadLine() ?? string.Empty);
+
+                } while (!continueWordUnscramble.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
+                !continueWordUnscramble.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+            } while (continueWordUnscramble.Equals("Y", StringComparison.OrdinalIgnoreCase));
+        }
+
+        private static void ExecuteScrambledWordsManualEntryScenario()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            throw new NotImplementedException();
         }
     }
 }
