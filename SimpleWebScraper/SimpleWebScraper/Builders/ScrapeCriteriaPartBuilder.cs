8d5680e941f2c305811f7aec073860f099ce6b01
Marvie Gasataya <marescanog@gmail.com>
2020-09-02 17:39:08 +0800

    Builder for ScrapeCriteriaPart

---
+++ SimpleWebScraper/SimpleWebScraper/Builders/ScrapeCriteriaPartBuilder.cs
@@ -0,0 +1,49 @@
+﻿using SimpleWebScraper.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleWebScraper.Builders
+{
+    class ScrapeCriteriaPartBuilder
+    {
+        private string _regex;
+        private RegexOptions _regexOptions;
+
+        ScrapeCriteriaPartBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _regex = string.Empty;
+            _regexOptions = RegexOptions.None;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegexOptions(RegexOptions regexoptions)
+        {
+            _regexOptions = regexoptions;
+            return this;
+        }
+
+        public ScrapeCriteriaPart Build()
+        {
+            ScrapeCriteriaPart scrapeCriteriaPart = new ScrapeCriteriaPart();
+            scrapeCriteriaPart.Regex = _regex;
+            scrapeCriteriaPart.RegexOption = _regexOptions;
+
+            return scrapeCriteriaPart;
+        }
+    }
+}
