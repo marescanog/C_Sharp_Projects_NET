37ec27715dcbf1de3b56ac2d893dd96bfdb2e517
Marvie Gasataya <marescanog@gmail.com>
2020-09-24 22:08:16 +0800

    Created skeleton methods and also wrote new strategy class

---
+++ SudokuSolver/SudokuSolver/Strategies/NakedPairStrategy.cs
@@ -0,0 +1,61 @@
+﻿using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Strategies
+{
+    class NakedPairStrategy : ISudokuStrategy
+    {
+        private readonly SudokuMapper _sudokuMapper;
+
+        public NakedPairStrategy(SudokuMapper sudokuMapper)
+        {
+            _sudokuMapper = sudokuMapper;
+        }
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(0); col++)
+                {
+                    EliminateNakedPairFromOthersInRow(sudokuBoard, row, col);
+                    EliminateNakedPairFromOthersInCol(sudokuBoard, row, col);
+                    EliminateNakedPairFromOthersInBlock(sudokuBoard, row, col);
+                }
+            }
+
+            return sudokuBoard;
+        }
+
+        private void EliminateNakedPairFromOthersInRow(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            if (!HasNakedPairInRow(sudokuBoard, givenRow, givenCol)) return;
+
+            for (int col = 0; col < sudokuBoard.GetLength(0); col++)
+            {
+                if (sudokuBoard[givenRow, col] != sudokuBoard[givenRow, givenCol] && sudokuBoard[givenRow, col].ToString().Length > 1)
+                {
+                    //EliminateNakedPair(sudokuBoard[givenRow, givenCol], givenRow, givenCol, col);
+                }
+            }
+        }
+
+        private bool HasNakedPairInRow(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EliminateNakedPairFromOthersInCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EliminateNakedPairFromOthersInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
