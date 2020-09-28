7e2a4fa952ee18a76da0a1d874144ca7ba518d58
Marvie Gasataya <marescanog@gmail.com>
2020-09-28 20:30:05 +0800

    Experimenting

--- BackendForTranscriptionChecker/BackendForTranscriptionChecker/Program.cs
+++ BackendForTranscriptionChecker/BackendForTranscriptionChecker/Program.cs
@@ -10,34 +10,62 @@ namespace BackendForTranscriptionChecker
     {
         static void Main(string[] args)
         {
-            string original = "The quick brown fox jumps over the lazy dog.";
-            string transcript = "The brown fox jumps over the lazy dog.";
+            string original = "The quick brown fox jumps over the lazy dog";
+            string transcript = "The brown quick fox jumps over the lazy dog";
 
-            string[] listOriginal = ChangeToUpperCaseAndSplitByWord(original);
-            string[] listTranscript = ChangeToUpperCaseAndSplitByWord(transcript);
+            List<string> listOriginal = SplitbyWordToUpper(original);
+            List<string> listTranscript = SplitbyWordToUpper(transcript);
 
+            //var listIntersection = listOriginal.Intersect(listTranscript);
+            //Console.WriteLine("test");
 
             //First Check if String is less than the original or more than the original
             //less than - gauranteed missing words
             //more than - added words
             //same - less than, missing or added
-
             
-            for(int i = 0; i < listOriginal.Length -1 ; i++)
+            for(int i = 0, j=0; i < listOriginal.Count -1 ; i++, j++)
             {
-                Console.WriteLine($"{listOriginal[i]} and {listTranscript[i]}");
+                Console.WriteLine($"{listOriginal[i]} and {listTranscript[j]}");
+
+                if(!listOriginal[i].Equals(listTranscript[j]))
+                {
+                    if(IsMissingWord(i, listOriginal, listTranscript))
+                    {
+                        Console.WriteLine($"Missing Word {listOriginal[i]}");
+                        j--;
+                    }
+                }
             }
             
+
         }
 
-        static string[] ChangeToUpperCaseAndSplitByWord(string phrase)
+        static List<string> SplitbyWordToUpper(string phrase)
         {
-            return phrase.ToUpper().Split(' ');
+            return phrase.ToUpper().Split(' ').ToList();
         }
 
-        static bool IsMissingWord(int index, string wordtocheck, string nextWord, string[] transcript)
-        {
 
+
+        //string original = "The quick brown fox jumps over the lazy dog.";
+        //string transcript = "The brown fox jumps quick over the lazy dog.";
+
+        static bool IsMissingWord(int index, List<string> original , List<string> transcript)
+        {
+            string wordtocheck = original[index];
+
+            if (!transcript.Contains(wordtocheck))
+            {
+                return true;
+            }
+
+            //Check if the next words are missing
+            //transcript.IndexOf(wordtocheck)
+            //index = 1
+            //indexWhereWordIsFoundInTranscript = 4
+
+            int indexWhereWordIsFoundInTranscript = transcript.IndexOf(wordtocheck);
 
             return true;
         }
