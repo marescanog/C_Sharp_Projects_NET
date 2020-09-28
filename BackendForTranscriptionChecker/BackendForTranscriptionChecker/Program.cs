837ffef97fa4f8f8e05bfe81848d00f79829c173
Marvie Gasataya <marescanog@gmail.com>
2020-09-28 19:04:28 +0800

    Wrote Skeleton

--- BackendForTranscriptionChecker/BackendForTranscriptionChecker/Program.cs
+++ BackendForTranscriptionChecker/BackendForTranscriptionChecker/Program.cs
@@ -10,6 +10,36 @@ namespace BackendForTranscriptionChecker
     {
         static void Main(string[] args)
         {
+            string original = "The quick brown fox jumps over the lazy dog.";
+            string transcript = "The brown fox jumps over the lazy dog.";
+
+            string[] listOriginal = ChangeToUpperCaseAndSplitByWord(original);
+            string[] listTranscript = ChangeToUpperCaseAndSplitByWord(transcript);
+
+
+            //First Check if String is less than the original or more than the original
+            //less than - gauranteed missing words
+            //more than - added words
+            //same - less than, missing or added
+
+            
+            for(int i = 0; i < listOriginal.Length -1 ; i++)
+            {
+                Console.WriteLine($"{listOriginal[i]} and {listTranscript[i]}");
+            }
+            
+        }
+
+        static string[] ChangeToUpperCaseAndSplitByWord(string phrase)
+        {
+            return phrase.ToUpper().Split(' ');
+        }
+
+        static bool IsMissingWord(int index, string wordtocheck, string nextWord, string[] transcript)
+        {
+
+
+            return true;
         }
     }
 }
